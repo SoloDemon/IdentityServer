f37fa6998feedb2198897837847b21fb4690c173
zhoufei <fqydhack@hotmail.com>
2020-04-18 14:27:59 +0800

    1.重新初始化项目

---
+++ IS4.AuthorizationCenter/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IS4.AuthorizationCenter.Models.Account
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "昵称")]
+        public string RealName { get; set; }
+
+        [Required]
+        [Display(Name = "登录名")]
+        public string LoginName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "邮箱")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "{0}必须和{2}一致,并且不能大于{1}个字符.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "确认密码")]
+        [Compare("Password", ErrorMessage = "密码和确认密码不一致")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Display(Name = "性别")]
+        public byte Sex { get; set; } = 0;
+
+        [Display(Name = "生日")]
+        public DateTime Birth { get; set; } = DateTime.Now;
+    }
+}
---
+++ IS4.AuthorizationCenter/Models/Entity/ApplicationUserRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace IS4.AuthorizationCenter.Models.Entity
+{
+    public class ApplicationUserRole : IdentityUserRole<Guid>
+    {
+        public virtual ApplicationUser User { get; set; }
+        public virtual ApplicationRole Role { get; set; }
+    }
+}
